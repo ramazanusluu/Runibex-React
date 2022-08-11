49c38510fae816b783b591002934d5a58ef73f54
Ramazan USLU <ruslu1720@gmail.com>
2022-08-11 15:38:50 +0300

    Section 6: React State Sistemi

--- hemisphere/src/App.js
+++ hemisphere/src/App.js
@@ -1,15 +1,46 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: null,
+      errorMessage: "",
+    };
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (error) => console.log(error)
+      (position) => {
+        this.setState({ latitude: position.coords.latitude });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
     );
-    return (
-      <div>
-        <h1>You are in the norhern hemisphere.</h1>
-      </div>
-    );
+  }
+  render() {
+    if (this.state.errorMessage && !this.state.latitude) {
+      return (
+        <>
+          <div>
+            <h1>{this.state.errorMessage}</h1>
+          </div>
+        </>
+      );
+    } else if (!this.state.errorMessage && this.state.latitude) {
+      return (
+        <>
+          <div>
+            <h1>{this.state.latitude}</h1>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div>
+            <h1>Loading...</h1>
+          </div>
+        </>
+      );
+    }
   }
 }
