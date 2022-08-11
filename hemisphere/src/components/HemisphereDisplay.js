e92471202fd8775f8f2401ad273fb6d434215f9d
Ramazan USLU <ruslu1720@gmail.com>
2022-08-11 13:31:45 +0300

    Section 5: Class-Based Components

---
+++ hemisphere/src/components/HemisphereDisplay.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HemisphereDisplay() {
+  return (
+    <div>
+      <h1>Hey you are in Northern hemisphere !</h1>
+    </div>
+  );
+}
+
+export default HemisphereDisplay;
