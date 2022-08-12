88a49640a66fa8977add2c0fc713a6bb5ecf03c1
Ramazan USLU <ruslu1720@gmail.com>
2022-08-12 15:25:05 +0300

    Section 10: React Router

---
+++ react-router/src/components/Contact.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div
+      className="ui raised very paddedtext container segment"
+      style={{ marginTop: "80px" }}
+    >
+      <h3 className="ui header">Contact Components</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+        corrupti alias repellat ratione incidunt minus suscipit non, earum
+        itaque illum voluptatum consequuntur est quia ex officia repellendus
+        dolorum debitis ab!
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
